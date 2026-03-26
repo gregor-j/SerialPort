1fe066c3b6c2175ea28aaf02ebe351ef31174c21
Gregor <gregor-j@users.noreply.github.com>
2026-03-26 17:33:16 +0100

    remove code from ReadException

--- src/Exceptions/ReadException.php
+++ src/Exceptions/ReadException.php
@@ -6,36 +6,8 @@ use Throwable;
 
 /**
  * Class ReadException
- * @package GregorJ\SerialPort\Exceptions
- * @author  Gregor J.
+ * Read exceptions are thrown in case reading from the stream failed.
  */
 class ReadException extends RuntimeException
 {
-    /**
-     * @var string
-     */
-    private $response;
-
-    /**
-     * Construct the exception. Note: The message is NOT binary safe.
-     * @link https://php.net/manual/en/exception.construct.php
-     * @param string          $response [optional] The response from the serial port until the exception is thrown.
-     * @param string          $message  [optional] The Exception message to throw.
-     * @param int             $code     [optional] The Exception code.
-     * @param Throwable|null $previous [optional] The previous throwable used for the
-     *                                  exception chaining.
-     */
-    public function __construct(string $response = "", string $message = "", int $code = 0, Throwable $previous = null)
-    {
-        $this->response = $response;
-        parent::__construct($message, $code, $previous);
-    }
-
-    /**
-     * @return string
-     */
-    public function getResponse(): string
-    {
-        return $this->response;
-    }
 }
