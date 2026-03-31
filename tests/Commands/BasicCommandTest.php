3ffe38b4a7c5f49de089880a0a66ef2ce2d839c2
Gregor <gregor-j@users.noreply.github.com>
2026-03-31 16:32:53 +0200

    add BasicCommandTest

---
+++ tests/Commands/BasicCommandTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\GregorJ\SerialPort\Commands;
+
+use GregorJ\SerialPort\Commands\BasicCommand;
+use GregorJ\SerialPort\Exceptions\InvalidValueException;
+use GregorJ\SerialPort\Exceptions\ReadException;
+use GregorJ\SerialPort\Exceptions\StateException;
+use GregorJ\SerialPort\Exceptions\TimeoutException;
+use GregorJ\SerialPort\Exceptions\UnexpectedResponseException;
+use GregorJ\SerialPort\Exceptions\WriteException;
+use GregorJ\SerialPort\Interfaces\Communication;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Test the BasicCommand class.
+ */
+class BasicCommandTest extends TestCase
+{
+    /**
+     * Test BasicCommand class.
+     * @return void
+     * @throws InvalidValueException
+     * @throws ReadException
+     * @throws StateException
+     * @throws TimeoutException
+     * @throws UnexpectedResponseException
+     * @throws WriteException
+     */
+    public function testBasicCommand(): void
+    {
+        $com = $this->getMockBuilder(Communication::class)->getMock();
+        $com->expects(static::once())
+            ->method('setTimeout')
+            ->with(1.0);
+        $com->expects(static::once())
+            ->method('write')
+            ->with('HELLO', "\n");
+        $com->expects(static::once())
+            ->method('read')
+            ->with("\r")
+            ->willReturn("WORLD\r");
+        $command = new BasicCommand('HELLO', "\n", "\r", 1.0);
+        $this->assertSame('HELLO\n', (string)$command);
+        $response = $command->invoke($com);
+        $this->assertSame("WORLD\r", $response->getRawResponse());
+        static::assertSame('WORLD', (string)$response);
+    }
+}
