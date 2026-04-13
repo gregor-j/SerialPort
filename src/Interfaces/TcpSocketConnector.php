a7f974cd39b9a0430514b90f1e68ea51e125b4bb
Gregor <gregor-j@users.noreply.github.com>
2026-04-13 15:10:39 +0200

    add new interfaces for abstracting PHP internal functions

---
+++ src/Interfaces/TcpSocketConnector.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GregorJ\SerialPort\Interfaces;
+
+/**
+ * Abstraction for opening a TCP socket stream.
+ */
+interface TcpSocketConnector
+{
+    /**
+     * Open Internet or Unix domain socket connection.
+     * @param string $hostname
+     * @param int $port
+     * @param int &$error_code
+     * @param string &$error_message
+     * @param float|null $timeout
+     * @return resource|false
+     */
+    public function connect(string $hostname, int $port, int &$error_code, string &$error_message, float|null $timeout);
+}
