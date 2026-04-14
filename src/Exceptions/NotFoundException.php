d7ac9b9fae96fa622ca9b481c0df66e0c524a63d
Gregor <gregor-j@users.noreply.github.com>
2026-04-14 14:32:48 +0200

    change Response to extend PSR-11 ContainerInterface

--- src/Exceptions/NotFoundException.php
+++ src/Exceptions/NotFoundException.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace GregorJ\SerialPort\Exceptions;
 
 use Exception;
+use Psr\Container\NotFoundExceptionInterface;
 
 /**
  * The NotFoundException is thrown if a value is expected but cannot be found.
  */
-final class NotFoundException extends Exception
+final class NotFoundException extends Exception implements NotFoundExceptionInterface
 {
 }
--- src/Interfaces/Response.php
+++ src/Interfaces/Response.php
@@ -4,31 +4,16 @@ declare(strict_types=1);
 
 namespace GregorJ\SerialPort\Interfaces;
 
-use GregorJ\SerialPort\Exceptions\NotFoundException;
+use Psr\Container\ContainerInterface;
 
 /**
  * The command defines which Response will be returned. The Response decides
  * how to interpret the raw response of the Command.
- * In case the constructor fails to interpret the raw response, throw an
- * UnexpectedResponseException.
+ * In case the constructor of the Response fails to interpret the raw response
+ * of the Command, throw an UnexpectedResponseException.
  */
-interface Response
+interface Response extends ContainerInterface
 {
-    /**
-     * Get a value from the Response.
-     * @param string $name The name of the value.
-     * @return mixed
-     * @throws NotFoundException
-     */
-    public function get(string $name): mixed;
-
-    /**
-     * Determine whether the Response contains a Value.
-     * @param string $name The name of the Value.
-     * @return bool
-     */
-    public function has(string $name): bool;
-
     /**
      * Transform the Response to a printable string for logging.
      * Non-printable characters are expected to be displayed as printable!
