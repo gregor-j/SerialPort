c060f11677a1059166086ef26ebf2cefafe72745
Gregor <gregor-j@users.noreply.github.com>
2026-04-14 11:05:13 +0200

    remove raw response from UnexpectedResponseException

--- src/Exceptions/UnexpectedResponseException.php
+++ src/Exceptions/UnexpectedResponseException.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace GregorJ\SerialPort\Exceptions;
 
 use Exception;
-use Throwable;
 
 /**
  * The UnexpectedResponseException is thrown in case the response didn't match
@@ -13,28 +12,4 @@ use Throwable;
  */
 final class UnexpectedResponseException extends Exception
 {
-    private mixed $rawResponse;
-
-    /**
-     * Construct the exception. Note: The message is NOT binary safe.
-     * @link https://php.net/manual/en/exception.construct.php
-     * @param string $message [optional] The Exception message to throw.
-     * @param int $code [optional] The Exception code.
-     * @param null|Throwable $previous [optional] The previous throwable used for the exception chaining.
-     * @param mixed $rawResponse [optional] The raw/unmodified response that lead to the exception.
-     */
-    public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null, mixed $rawResponse = null)
-    {
-        $this->rawResponse = $rawResponse;
-        parent::__construct($message, $code, $previous);
-    }
-
-    /**
-     * Get the raw/unmodified response, that lead to the exception.
-     * @return mixed
-     */
-    public function getRawResponse(): mixed
-    {
-        return $this->rawResponse;
-    }
 }
